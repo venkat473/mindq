dd44a884537d8e32a99ca3d54825dfa169529881
LENOVO <LENOVO@DESKTOP-32RJ7DN>
2019-07-11 16:51:37 +0530

    first commit

---
+++ selenium/src/test/java/programs/Prog1.java
@@ -0,0 +1,17 @@
+package programs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Prog1 
+{
+
+	public static void main(String[] args)
+	{
+		WebDriver driver=new ChromeDriver();
+		 driver.get("https://mail.google.com");
+	
+
+	}
+
+}
